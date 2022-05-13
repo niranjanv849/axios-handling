76c4d90f3ddc3cdf16189ebf7222ca7efedb3159
niranjanv849 <niranjanv849@gmail.com>
2022-05-13 16:31:04 +0530

    Header UI 🎨✅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="text-center">
+          <h1 className="display-4 text-center mb-3">Axios Crash Course</h1>
+          <button className="btn btn-primary my-3" id="get">
+            GET
+          </button>
+          <button className="btn btn-info" id="post">
+            POST
+          </button>
+          <button className="btn btn-warning" id="update">
+            PUT/PATCH
+          </button>
+          <button className="btn btn-danger" id="delete">
+            DELETE
+          </button>
+          <button className="btn btn-secondary" id="sim">
+            Sim Requests
+          </button>
+          <button className="btn btn-secondary" id="headers">
+            Custom Headers
+          </button>
+          <button className="btn btn-secondary" id="transform">
+            Transform
+          </button>
+          <button className="btn btn-secondary" id="error">
+            Error Handling
+          </button>
+          <button className="btn btn-secondary" id="cancel">
+            Cancel
+          </button>
+        </div>
+        <hr />
+        <div id="res"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
